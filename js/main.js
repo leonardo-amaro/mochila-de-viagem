b7751c37b0239e9f338b7532083ac3df5003507b
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2023-01-14 10:21:34 -0300

    adicionado function de criar item

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const form = document.querySelector("#novoItem");
+const lista = document.querySelector(".lista");
+
+function criarItem(quantidade, nome) {
+  let li = document.createElement("li");
+  li.classList.add("item");
+  li.innerHTML = `<strong>${quantidade}</strong>${nome}`;
+  lista.appendChild(li);
+};
 
 form.addEventListener("submit", evento => {
   evento.preventDefault(); // Evita que o formulário seja enviado e "resetado" antes de ser usado
 
-  console.log(evento.target.elements["nome"].value)
-  console.log(evento.target.elements["quantidade"].value)
+  criarItem(evento.target.elements["quantidade"].value, evento.target.elements["nome"].value)
 });
