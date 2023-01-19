c623ec429bdff5a35e7b6eeb3aa106389f47118c
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2023-01-19 15:30:33 -0300

    mochila-de-viagem

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
+/* -Referências--------- */
 const form = document.querySelector("#novoItem");
 const lista = document.querySelector("#lista");
-const itens = [];
+const itens = JSON.parse(localStorage.getItem("itens")) || []; // Converter a string do Local Storage em JSON (parse) e armazenar
 
-function criarItem(quantidade, nome) {
-  let li = document.createElement("li");
-  li.classList.add("item");
-  li.innerHTML = `<strong>${quantidade}</strong>${nome}`;
-  lista.appendChild(li);
-
+/* -Funcionalidades--------- */
+function armazenarItem(quantidade, nome) {
   const itemAtual = {
     "nome": nome,
     "quantidade": quantidade
   };
 
   itens.push(itemAtual);
-
-  localStorage.setItem("item", JSON.stringify(itens))
+  localStorage.setItem("itens", JSON.stringify(itens)) // Converter o array de objetos em string para armazenar no Local Storage
 };
+
+function criarItem(quantidade, nome) {
+  let li = document.createElement("li");
+  li.classList.add("item");
+  li.innerHTML = `<strong>${quantidade}</strong>${nome}`;
+  lista.appendChild(li);
+};
+
+function atualizarLista() {
+  lista.innerHTML = "";
+  itens.forEach(elemento => {
+    criarItem(elemento.quantidade, elemento.nome)
+  })
+};
+
+/* -Eventos--------- */
+document.addEventListener("DOMContentLoaded", () => atualizarLista());
 
 form.addEventListener("submit", evento => {
   evento.preventDefault(); // Evita que o formulário seja enviado e "resetado" antes de ser usado
@@ -24,7 +37,8 @@ form.addEventListener("submit", evento => {
   const quantidade = evento.target.elements["quantidade"];
   const nome = evento.target.elements["nome"];
 
-  criarItem(quantidade.value, nome.value);
+  armazenarItem(quantidade.value, nome.value);
+  atualizarLista();
 
   quantidade.value = ""; // Limpar campo "quantidade"
   nome.value = "" // Limpar campo "nome"
