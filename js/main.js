a32f694c1f520735c598f6b025a762c69833ab80
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2023-01-19 14:26:08 -0300

    trabalhando com localStorage

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 const form = document.querySelector("#novoItem");
 const lista = document.querySelector("#lista");
+const itens = [];
 
 function criarItem(quantidade, nome) {
   let li = document.createElement("li");
   li.classList.add("item");
   li.innerHTML = `<strong>${quantidade}</strong>${nome}`;
   lista.appendChild(li);
+
+  const itemAtual = {
+    "nome": nome,
+    "quantidade": quantidade
+  };
+
+  itens.push(itemAtual);
+
+  localStorage.setItem("item", JSON.stringify(itens))
 };
 
 form.addEventListener("submit", evento => {
   evento.preventDefault(); // Evita que o formulário seja enviado e "resetado" antes de ser usado
 
-  criarItem(evento.target.elements["quantidade"].value, evento.target.elements["nome"].value)
+  const quantidade = evento.target.elements["quantidade"];
+  const nome = evento.target.elements["nome"];
+
+  criarItem(quantidade.value, nome.value);
+
+  quantidade.value = ""; // Limpar campo "quantidade"
+  nome.value = "" // Limpar campo "nome"
 });
